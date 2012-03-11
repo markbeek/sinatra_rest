846a5857e7b1a8dc05a5ff84158601bdca9113c6
Mark Buechler <markbeek@carolina.rr.com>
2012-03-11 18:56:29 -0400

    changes

--- test/test_yaml_dao.rb
+++ test/test_yaml_dao.rb
@@ -1,5 +1,16 @@
-require 'unit/test'
+#run raw with
+#ruby -I. test/test_yaml_dao.rb
 
-def test_add
-	assert(1==1)
+#run rake with:
+#rake test
+
+require 'test/unit'
+require_relative '../lib/dao/yaml_dao.rb'
+
+class YamlDaoTest < Test::Unit::TestCase
+
+	def test_add_person
+		assert(1==1)
+	end
+
 end
