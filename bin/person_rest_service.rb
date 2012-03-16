e242ab53891d86ea8da8df7d8e912fa0e3af6f5e
Mark Buechler <markbeek@carolina.rr.com>
2012-03-15 23:39:47 -0400

    c

--- bin/person_rest_service.rb
+++ bin/person_rest_service.rb
@@ -5,13 +5,21 @@ require_relative '../lib/dao/yaml_dao'
 
 puts "starting person service"
 
-DATA_FILE = 'data/persons.yaml'
+#"production" data file
+DEFAULT_DATA_FILE = 'data/persons.yaml'
 
-puts "ARGV[0]: #{ARGV[0]}"
+#we may pass in a test data file,
+#which we will use,
+#or rake may pass in a file as part of its file list,
+#which we want to ignore
+data_file = DEFAULT_DATA_FILE
+if ARGV[0] && ARGV[0].match(/yaml$/)
+	data_file = ARGV[0]
+end
 
 before do
-	#content_type "application/json"	#both browsers treat this as a download, have to open in separate application 
-	content_type :txt	#this is rendered directly in the browser
+	content_type "application/json"	#both browsers treat this as a download, have to open in separate application 
+	#content_type :txt	#this is rendered directly in the browser
 end
 
 get '/persontest' do
@@ -19,16 +27,19 @@ get '/persontest' do
 end
 
 get '/person/:id' do
-	person_dao = YamlDao.new(DATA_FILE)
+	person_dao = YamlDao.new(data_file)
 	person_data = person_dao.retrieve(params[:id])
 	person_json = JSON.generate(person_data) 
 end
 
 post '/person' do
+	puts "post params object"
 	p params
-	#person_dao = YamlDao.new(DATA_FILE)
+	puts "request body: #{request.body.read}"
+	puts
+	#person_dao = YamlDao.new(data_file)
 	#person_dao.create(params[:id],{:name => params[:name], :age => params[:age]})
-	"OK"
+	JSON.generate({"url" => "/person/pmayber"})
 end
 
 puts "person service started"
--- test/test_person_rest_service.rb
+++ test/test_person_rest_service.rb
@@ -7,21 +7,56 @@ require_relative "../bin/person_rest_service.rb"
 require 'test/unit'
 require 'rack/test'
 require 'json'
+require 'rest_client'
+
+DATA_FILE = 'test/baseline_persons.yaml'
 
 class PersonServiceTest < Test::Unit::TestCase
 
 	include Rack::Test::Methods
 	
+	#write a clean a file with known data before each test
+	def setup
+		puts "setting up baseline file"
+		system("ruby -I. test/baseline_person_resource_generator.rb")
+		puts "baseline file is set up"
+		@yaml_dao = YamlDao.new(DATA_FILE)
+	end
+	
+	#delete file
+	def teardown
+		@yaml_dao = nil
+		num_files_deleted = File.delete(DATA_FILE)
+		assert_equal(1,num_files_deleted)
+	end
+	
 	def app
 		Sinatra::Application
 	end
 	
-	def test_get
+	def test_get_persontext
 		get '/persontest'
-		puts last_response.body
+		puts "test_get_persontext, last_response.body: #{last_response.body}"
 		hash = JSON.parse(last_response.body)
 		assert_equal 200, last_response.status
 		assert_equal 'Mary', hash['name']
+	end
+	
+	def test_get_person
+		get '/person/cabbot'
+		puts "test_get_person, last_response.body: #{last_response.body}"
+		person_hash = JSON.parse(last_response.body)
+		assert_equal 200, last_response.status
+		assert_equal "Cindy Abbot", person_hash['name']
+		assert_equal 21, person_hash['age']
+	end
+	
+	def test_post
+		req = JSON.generate({"name" => "Priscilla Mayberry", "age" => 23})
+		post '/person', req
+		response_hash = JSON.parse(last_response.body)
+		assert_equal 200, last_response.status
+		assert_equal "/person/pmayber", response_hash['url']
 	end
 
 end
